c1567faebe2331871eb6f3c16880424fdf9f837c
matthewdavidr12 <matthewdavidr12@hotmail.com>
2023-03-28 16:49:36 -0500

    all good

---
+++ src-es6/00-let-const.js
@@ -0,0 +1,25 @@
+var lastname = "David";
+lastname = "Oscar";
+console.log(lastname);
+
+
+let fruit = "apple";
+fruit = "kiwi";
+console.log(fruit);
+
+
+const animal = "dog";
+animal = "cat";
+
+
+const fruits = () => {
+if(true) {
+    var fruit1 = "apple"; //funtion scope
+    let fruit2 = "kiwi"; // blick scope
+    const fruit3 = "banana"; // block scope 
+}
+console.log (fruit1)
+console.log (fruit2)
+console.log (fruit3)
+}
+
